409c9e8f7dd8fcad01180ff86b3dfb1d8525863b
Gabriela Paternoste <121585938+trumanblack00@users.noreply.github.com>
2022-12-29 19:41:24 -0300

    NavBar

---
+++ NavBar.js
@@ -0,0 +1,27 @@
+
+
+import React from 'react'
+import './NavBar.css'
+
+
+
+import CardWidget from '../cardWidget/CardWidget.js'
+
+const NavBar = () => {
+
+    return(
+        <header className='navbar'>
+        <h1 className='title'>DALT</h1>
+        <ul className='links'>
+          <li><a href="index.html">Productos</a></li>
+          <li><a href="index.html">Ofertas</a></li>
+          <li><a href="index.html">Envios</a></li>
+        </ul>
+        <CardWidget cantidad="7"/>
+        
+      </header>
+    )
+    
+}
+
+export default NavBar
