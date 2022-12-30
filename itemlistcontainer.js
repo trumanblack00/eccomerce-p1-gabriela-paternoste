dc827f3e30cfa29bfe1f4bfc4d21a83e264e93d8
Gabriela Paternoste <121585938+trumanblack00@users.noreply.github.com>
2022-12-30 17:32:08 -0300

    Update itemlistcontainer.js

--- itemlistcontainer.js
+++ itemlistcontainer.js
@@ -1,25 +1,15 @@
 
 import React from 'react'
-//Modulos
-//Estilos
 import './ItemListContainer.css'
-//Componentes
-//Core
 
-
-//logica//
-
-//Funcion constructora
 const ItemListContainer = (props) => {
     //Retorno que se va a renderizar
     return(
-        <div className='greeting'>
-            Aqui va el ItemListContainer: {props.greeting}
+        <div className='bienvenidos'>
+            Aqui va el ItemListContainer: {props.bienvenidos}
         </div>
     )
     
 }
-
-
 
 export default ItemListContainer
