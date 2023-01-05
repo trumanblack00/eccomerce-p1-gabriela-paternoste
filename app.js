d386109b645064842de470c0621bec33f795dc8b
Gabriela Paternoste <121585938+trumanblack00@users.noreply.github.com>
2023-01-04 21:18:59 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+function App () {
+  return ( 
+      
+    <div>
+    <header>
+       <nav>
+         <a>Home </a>
+         <a>Productos</a>
+         <a>Ofertas</a>
+         <a>Envios</a>
+        </nav>
+    </header>
+        <main>
+         <p> CONTENIDO </p>
+        </main>
+        <footer>
+         <p>Si pinta, pinta cerveria artesanal - contacto</p>
+       </footer>
+    </div>
+  );
+  
+}
+
+export default App;
